1ffd8f5919e87f70895fe2e4fa539ba8c53f9d2a
AminaChakirDev <amina.aitm@gmail.com>
2022-06-28 13:58:39 +0200

    Install react router dom and create page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
